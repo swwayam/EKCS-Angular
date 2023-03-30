ba41a463fe4fce78bae29801c2ab0f6dcb3d3285
Rahulusd1 <rahulusd1@gmail.com>
2023-03-30 20:36:38 +0530

    new

---
+++ Rahul/Functions.js
@@ -0,0 +1,13 @@
+//function sumArr() {
+    //var arr = [1,2,53,2,53,23,5,3];
+   // let sum = 0;
+  
+    //for (let i = 0; i < arr.length; i += 1) {
+     // sum += arr[i];
+   // }  
+   // return sum;
+  //}
+  //console.log(sumArr([1,2,53,2,53,23,5,3]));  
+  let valuees=[1,22,22,22,22,22,1,23,43,2,4,2]
+
+  const newValues =
