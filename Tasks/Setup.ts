e71eb99ba035105e80b5c4bfa30a92ab7870979f
swwayam <codingswayam24@gmail.com>
2023-04-06 19:28:25 +0530

    Task 2

--- Tasks/Setup.ts
+++ Tasks/Setup.ts
@@ -1,3 +1,7 @@
 // 1. Download type script for your system.
 // 2. https://www.typescriptlang.org/download
 // 3. https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+
+
+// TO create an interface of EKCS Employe details
+// Implement it in an object the blood grp should be optional field
